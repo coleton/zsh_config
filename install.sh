118a6deba7dbeabadbc0f8530e8b79cec7bab6c8
Coleton Pierson <coleton.pierson@praetorian.com>
2015-02-13 14:07:43 -0600

    initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+path=$(pwd)
+cp ./vimrc ~/.vimrc
+cd ~/
+curl -L http://install.ohmyz.sh | sh
+sed 's/^ZSH_THEME.*/ZSH_THEME="pygmalion"/' ~/.zshrc
+echo 'alias c="clear"' | tee -a ~/.zshrc
+echo 'alias e="exit"' | tee -a ~/.zshrc
+mkdir -p ~/.vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
