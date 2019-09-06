2418314c1c96d985689d6353e2a0e09b484329ee
Coleton Pierson <coleton.pierson@gmail.com>
2019-09-06 11:23:33 +0700

    Added aliases, functions, and initializer

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ cp ./.vimrc ~/.vimrc
 cd ~/
 curl -L http://install.ohmyz.sh | sh
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="pygmalion"/' ~/.zshrc
-echo 'alias c="clear"' | tee -a ~/.zshrc
-echo 'alias e="exit"' | tee -a ~/.zshrc
 echo 'export TERM=xterm-256color' | tee -a ~/.zshrc
 echo 'termcapinfo xterm-color|xterm|xterms|xs|rxvt ti@:te@' | tee -a ~/.screenrc
 echo 'bce "on"' | tee -a ~/.screenrc
@@ -20,3 +18,5 @@ mkdir -p ~/.oh-my-zsh/custom/plugins/screen/
 cd $path
 cp ./screen.plugin.zsh ~/.oh-my-zsh/custom/plugins/screen/screen.plugin.zsh
 sed -i 's/^plugins=(/plugins=(screen /' ~/.zshrc
+cp -r .coleton ~/
+echo ". ~/.coleton/init" | tee -a ~/.zshrc
