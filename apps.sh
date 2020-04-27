4377f43c24a0be1a7239238821541a084f622292
Coleton Pierson <coleton.pierson@gmail.com>
2020-04-27 17:43:52 +0700

    feat: added additional apps for installation.

--- apps.sh
+++ apps.sh
@@ -2,11 +2,13 @@
 
 current_dir=$(pwd)
 
+# Install RVM
 if [[ $(command -v rvm) == "" ]]; then
   echo "Installing Ruby Version Manager..."
   curl -sSL https://get.rvm.io | bash -s stable --ruby
 fi
 
+# Install Homebrew
 if [[ $(command -v brew) == "" ]]; then
   echo "Installing Hombrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -15,5 +17,11 @@ else
   brew update
 fi
 
+# Setup brewed apps
 cd $current_dir/brew/
 brew bundle
+
+# Install commitizen
+npm install -g commitizen
+npm install -g cz-conventional-changelog
+echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
